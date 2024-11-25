13d67794ebc8be639dd40a34dbf17c6933f8cab9
anmolsharma2001 <144166824+anmolsharma2001@users.noreply.github.com>
2024-11-26 00:28:34 +0530

    Update Extracurricular.jsx

--- src/components/ui/Extracurricular.jsx
+++ src/components/ui/Extracurricular.jsx
@@ -4,11 +4,11 @@ const Extracurricular = () => {
   return (
     <section id="extracurricular" className="mt-8">
       <div className="">
-        <h2 className="text-xl font-semibold text-gray-900 mb-8">Extracurricular Activities</h2>
+        <h2 className="text-lg font-semibold text-gray-900 mb-8">Extracurricular Activities</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           {/* Activity 1 */}
           <div className="bg-purple-200 p-6 rounded-lg shadow-md hover:shadow-lg transition-all">
-            <h3 className="text-xl font-semibold text-gray-900 mb-2">National Service Scheme Volunteer</h3>
+            <h3 className="text-lg font-semibold text-gray-900 mb-2">National Service Scheme Volunteer</h3>
             {/* <p className="text-gray-600">
               Volunteered at a local NGO, organizing events, fundraising, and supporting community programs to
               empower underprivileged children.
