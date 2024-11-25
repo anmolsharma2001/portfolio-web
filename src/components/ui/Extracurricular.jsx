eb9b28f58c0f6d28c7f46c4c554f42b0b4c5f0d9
anmolsharma2001 <144166824+anmolsharma2001@users.noreply.github.com>
2024-11-26 00:30:07 +0530

    Update Extracurricular.jsx

--- src/components/ui/Extracurricular.jsx
+++ src/components/ui/Extracurricular.jsx
@@ -4,7 +4,7 @@ const Extracurricular = () => {
   return (
     <section id="extracurricular" className="mt-8">
       <div className="">
-        <h2 className="text-lg font-semibold text-gray-900 mb-8">Extracurricular Activities</h2>
+        <h2 className="text-xl font-semibold text-gray-900 mb-8">Extracurricular Activities</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           {/* Activity 1 */}
           <div className="bg-purple-200 p-6 rounded-lg shadow-md hover:shadow-lg transition-all">
@@ -16,7 +16,7 @@ const Extracurricular = () => {
           </div>
           {/* Activity 2 */}
           <div className="bg-yellow-100 p-6 rounded-lg shadow-md hover:shadow-lg transition-all">
-            <h3 className="text-xl font-semibold text-gray-900 mb-2">Student Coordinator,<h5 className="text-sm">Student Council CSE, Medi-Caps University</h5></h3>
+            <h3 className="text-lg font-semibold text-gray-900 mb-2">Student Coordinator,<h5 className="text-sm">Student Council CSE, Medi-Caps University</h5></h3>
             {/* <p className="text-gray-600">
               Participated in several hackathons, collaborating with cross-functional teams to create innovative
               solutions within tight deadlines.
